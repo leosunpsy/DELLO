b3e783db78c7e353a0e69c2e39ee93703fdfe27e
Yanliang Sun <yanliangsun@126.com>
2024-03-14 23:16:19 +0800

    Update coordinate_calculator_loop.m

--- Functions/coordinate_calculator_loop.m
+++ Functions/coordinate_calculator_loop.m
@@ -1,22 +1,27 @@
 function coordinates_matrix = coordinate_calculator_loop(a,b,c,x,y,z,n)
 % This function is used for generate the coordinate matrix for each
 % contact, a b c are the tip coordinate of each electrode
-% x y z are the direction coordinate, in bioimmage suite, you may use the
-% value of i j k.
-% n is the number of contact on each electrode, because the first contact
-% does not need to be calculated, so when you do the loop, it should be
-% like for i = 1:n-1, and the first contact coordinate has already be
-% assigned to the matrix before hand.
+% x y z are the entry coordinate, you can pick a out skull point on 
+% the same electrode, in bioimmage suite, you may use the value of i j k.
+% n is the number of contact on each electrode
+% 1 unit = 1mm
+% diameter of the depth electrode: 0.8mm
+% contact length 2mm
+% space between 2 contacts 1.5mm
+% space btween two contacts center: 3.5mm /3.5 unit
 
-% initialize the coordinates_matrix
-coordinates_matrix = zeros(n,3);
-coordinates_matrix(1,:) = [a b c];
-% calculate the fill the coordinates_matrix
-for i = 1:n-1
-    coordinates_matrix(i+1,:) = coord_calc(a,b,c,x,y,z,i);
-end
-end
+% calcute the coordinates of all contacts on a single electrode 
+% using an automated method
+shaft_distance = 3.5;
+target_pos = [a, b, c];
+entry_pos = [x, y, z];
+ori = entry_pos - target_pos;
+ori = ori/norm(ori);
 
+coordinates_matrix = zeros(n, 3);
+for i = 1:n
+    coordinates_matrix(i,:) = target_pos + (i-1) * ori * shaft_distance;
+end
 
 %%write the coordinates in string format
 % L = fprintf(strcat(num2str(a),32,num2str(b),32,num2str(c)));
